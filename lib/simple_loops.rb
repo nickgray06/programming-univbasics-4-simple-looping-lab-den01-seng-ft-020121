468d5bb6f7190e6c33fbdaff7721de959024c728
Nick Gray <nickgray87@gmail.com>
2021-01-15 21:50:32 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -26,7 +26,7 @@ def return_string_array(array = ['hello', 'there', 'general', 'kenobi'])
   counter = 0
   new_array = []
   while counter < array.length do
-    return new_array.push(array[counter].to_s)
+    new_array.push(array[counter].to_s)
     counter += 1 
   end
   new_array
